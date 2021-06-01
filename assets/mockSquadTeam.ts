d7d39a485e96bf5bfacfc2f5d88148653a996c96
mchouaha <m.chouaha@gmail.com>
2021-06-01 11:09:36 +0200

    change data mock squad team

--- assets/mockSquadTeam.ts
+++ assets/mockSquadTeam.ts
@@ -11,71 +11,61 @@ export const squadTeam: any [] = [
         carica di adrenalina pazzesca.`,
         `Fra le mie sessioni di gioco, ricordo con piacere le vincite con Pitbull 
         alla <strong>Legacy of Dead</strong>, con Lucky alla <strong>Sweet Bonanza</strong> e con Chily a <strong>Vinci la Gallina…</strong> sono sicuro che 
-        presto sbancherò con Mr. Value, e perché no alla <strong>Roulette</strong>.
+        presto sbancherò con Mr. Value, e perché no alla <strong>Roulette</strong>.<br/>
         Sono stato accostato alla figura centrale della serie tv spagnola, non è un caso che anche io abbia 
         sempre pronto un piano, in questo caso una telefonata in diretta, per far sbancare il resto della 
         Squad. Quando qualcuno ha da dire sulle mie giocate, io risponderò con un secco: 
-        <strong>“Shhh, I’m mister J”</strong>. E non imitatemi… non fatelo a casa!`],
+        <strong>“I’m mister J, shhh”</strong>. E non imitatemi… non fatelo a casa!`],
         thumbnail:'/jpeg/squad/mrj.jpeg' 
     },
     { 
         name: "CHILY", 
-        info: [`Avete mai sentito parlare di un peperoncino rosso che gioca alle slots? Ebbene sì, esiste e si 
-        chiama Chily. Le mie giornate sono in parte occupate dalla mia impresa di produzione di marmi e 
-        da divertentissime lezioni di fisica agli studenti, ma trovo del tempo per divertirvi insieme a voi col 
-        casinò online. Quando sono live, faccio sempre un salto alla Honey Rush, Legacy of Dead, ed 
-        ultimamente ho scoperto di essermi innamorato alla follia della Pillars of Asgard… chissà perché!`,
-        `Le vincite più belle in assoluto sono state durante le sessioni alla <strong>Rise of the Mountain Kin</strong> e alla 
-        <strong>Pillars of Asgard</strong>. Non avendo la fortuna di Lucky, posso dire di lottare sempre contro la sfortuna.
-        Per alimentare le mie chance di vittoria, mangio sempre qualche peperoncino piccantissimo (ci 
-        sono clip che lo testimoniano). Ogni volta che sento dire: “Esci che non paga” risponderò con un 
-        amplificato: <strong>“Chi ha detto di uscire da qua?”</strong>.
-        Ad ogni pagata, tra sedie che volano e signore che sbattono la scopa sono certo che non potrete 
-        fare a meno di me. Vi aspetto!`],
+        info: [`Avete mai sentito parlare di un peperoncino rosso che gioca alle slots? Ebbene sì,
+        esiste e si chiama Chily. Le mie giornate sono in parte occupate dalla mia impresa di produzione di marmi
+        e da divertentissime lezioni di fisica agli studenti, ma trovo del tempo per divertirvi insieme a voi col casinò online.
+        Quando sono live, faccio sempre un salto alla Honey Rush, Legacy of Dead, ed ultimamente ho scoperto di essermi
+        innamorato alla follia della Pillars of Asgard… chissà perché!`,
+        `Le vincite più belle in assoluto sono state durante le sessioni alla <strong>Rise of the Mountain King</strong> e alla
+        <strong>Pillars of Asgard</strong>. Non avendo la fortuna di Lucky, posso dire di lottare sempre contro la sfortuna.<br/>
+        Per alimentare le mie chance di vittoria, mangio sempre qualche peperoncino piccantissimo (ci sono clip che lo testimoniano).
+        Ogni volta che sento dire: “Esci che non paga” risponderò con un amplificato: “<strong>Chi ha detto di uscire da qua?</strong>”.<br/>
+        Ad ogni pagata, tra sedie che volano e signore che sbattono la scopa sono certo che non potrete fare a meno di me. Vi aspetto!`],
         thumbnail:'/jpeg/squad/chily.jpeg' 
     },
     { 
         name: "PITBULL", 
-        info: [`Attenti a non farmi arrabbiare perché … io sono Pitbull! Il mio nome deriva dal fatto che possiedo 
-        le caratteristiche dell’omonimo cane. La forza, insieme alla tenacia mi hanno permesso di ottenere 
-        un sacco di soddisfazioni in ambito lavorativo, infatti, sono occupato su più fronti fra cui il turismo, 
-        il settore dell’abbigliamento e… vorrei coronare il sogno di diventare pilota d’aereo. Gioco spesso 
-        a qualsiasi tipo di Book, alla Reactoonz ed al <strong>Monopoly Live</strong> e al Blackjack.`,
-        `Ho dei bellissimi ricordi anche con la <strong>White Rabbit</strong>, <strong>la Moon Princess</strong> ecc… fortunato sì,
-        ma so gestire maniacalmente il bet.Si narra che le mie numerose vincite siano stato accompagnate dalla turbochiappa, che è 
-        candidata a diventare patrimonio dell’UNESCO. Ogni volta che sento dire: “E che cos’è….” 
-        prontamente rispondo: “<strong>LA SCOTECAAA</strong>”. Durante le mie migliori vincite sentirete sempre: 
-        “<strong>VAMONOOOS</strong>” `],
+        info: [`Attenti a non farmi arrabbiare perché … io sono Pitbull! Il mio nome deriva dal fatto che possiedo le
+        caratteristiche dell’omonimo cane. La forza, insieme alla tenacia mi hanno permesso di ottenere un sacco di
+        soddisfazioni in ambito lavorativo, infatti, sono occupato su più fronti fra cui il turismo,
+        il settore dell’abbigliamento e… vorrei coronare il sogno di diventare pilota d’aereo.
+        Gioco spesso a qualsiasi tipo di Book, alla Reactoonz ed al Monopoly Live e al Blackjack.
+        Ho dei bellissimi ricordi anche con la White Rabbit, la Moon Princess ecc… fortunato sì, ma so gestire maniacalmente il bet.`,
+        `Si narra che le mie numerose vincite siano stato accompagnate dalla turbochiappa, che è candidata a diventare patrimonio dell’UNESCO.
+        Ogni volta che sento dire: “E che cos’è….” prontamente rispondo: “<strong>LA SCOTECAAA</strong>”. Durante le mie migliori vincite sentirete sempre: “<strong>VAMONOOOS</strong>”`
+    ],
         thumbnail:'/jpeg/squad/pitbull.jpeg' 
     },
     { 
         name: "LUCKY", 
-        info: [`Ciao ragazzi, io sono Lucky e da qualche tempo ho guadagnato il soprannome di Lucky il fortunato, 
-        o meglio ancora il fortu-dado. Nella vita sono un autista di bus e lavoro nel settore del turismo, 
-        come hobby sono un ballerino da più di 10 anni e m occupo anche di videomaking. Vado d’accordo 
-        con tutto il resto della Squad, tranne che con Chily… scherzi a parte, siamo molto amici, solo che 
-        lui è un tipo molto scaramantico mentre io mi affido totalmente alla fortuna.`,
-        `Adoro i dadi e qualunque gioco li comprenda; mi piace il Monopoly Live ed il Crazy Time, ho un feeling particolare 
-        con le slots a tema egizio. Le mie vincite più interessanti sono state alla <strong>Sweet Bonanza</strong>, al 
-        <strong>Monopoly Live</strong> e alla <strong>Lightning Dice</strong>; sul canale YouTube trovate il resto. 
-        Il mio: “<strong>Sono veramente egizio</strong>” è diventato un detto conosciuto ai più, ed inserito anche dalla 
-        Crusca in tutti i dizionari della lingua italiana.
-        Sono un tipo pazzerello e non mi tiro mai indietro se mi viene chiesto dai followers di cantare o 
-        ballare. Bella raga, ci si vede in live.`],
+        info: [`Ciao ragazzi, io sono Lucky e da qualche tempo ho guadagnato il soprannome di Lucky il fortunato, o meglio ancora il fortu-dado.
+        Nella vita sono un autista di bus e lavoro nel settore del turismo, come hobby sono un ballerino da più di 10 anni e m occupo anche di videomaking.
+        Vado d’accordo con tutto il resto della Squad, tranne che con Chily… scherzi a parte, siamo molto amici,
+        solo che lui è un tipo molto scaramantico mentre io mi affido totalmente alla fortuna. Adoro i dadi e qualunque gioco li comprenda;
+        mi piace il Monopoly Live ed il Crazy Time, ho un feeling particolare con le slots a tema egizio.
+        Le mie vincite più interessanti sono state alla Sweet Bonanza, al Monopoly Live e alla Lightning Dice; sul canale YouTube trovate il resto.`,
+        `Il mio: “<strong>Sono veramente egizio</strong>” è diventato un detto conosciuto ai più, ed inserito anche dalla Crusca in tutti i dizionari della lingua italiana.<br/>
+        Sono un tipo pazzerello e non mi tiro mai indietro se mi viene chiesto dai followers di cantare o ballare. Bella raga, ci si vede in live.`],
         thumbnail:'/jpeg/squad/lucky.jpeg' 
     },
     { 
-        name: "MR VALUE", 
-        info: [`Salve a tutti, mi presento, sono Mr. Value e mi occupo di forniture di arredo scolastico ed urbano
-        in giro per il Mondo. Insieme agli altri ragazzi della Squad, vi farò compagnia quotidianamente fra 
-        una slot ed un gioco di casinò live. La mia slot preferita è la <strong>Book of Dead</strong>, anche se preferisco di 
-        gran lunga i giochi live come Blackjack e Roulette.`,
-        `Personalmente sono ancora alla ricerca della grande sbancata, per ora posso ritenermi soddisfatto
-        delle vincite alla <strong>Book of Dead</strong> e al Monopoly Live.
-        Ogni volta che decido di entrare al Monopoly Live becco sempre il momento in cui sono appena 
-        usciti i 4 rolls, infatti, è celebre la frase :”Dovevo entrare 2 minuti prima!”
-        <strong>Sono un abitante della Papuasia</strong> e gioco al BJ come scusa per cimentare le dealer… ops di più non 
-        posso dirvi, vi aspetto in live!`],
+        name: "VALUE", 
+        info: [`Salve a tutti, mi presento, sono Mr. Value e mi occupo di forniture di arredo scolastico ed urbano in giro per il Mondo.
+        Insieme agli altri ragazzi della Squad, vi farò compagnia quotidianamente fra una slot ed un gioco di casinò live.
+        La mia slot preferita è la <strong>Book of Dead</strong>, anche se preferisco di gran lunga i giochi live come Blackjack e Roulette.
+        Personalmente sono ancora alla ricerca della grande sbancata, per ora posso ritenermi soddisfatto delle vincite alla
+        <strong>Book of Dead e al Monopoly Live</strong>.`,
+        `Ogni volta che decido di entrare al Monopoly Live becco sempre il momento in cui sono appena usciti i 4 rolls, infatti, è celebre la frase :”Dovevo entrare 2 minuti prima!”
+        <strong>Sono un abitante della Papuasia</strong> e gioco al BJ come scusa per cimentare le dealer… ops di più non posso dirvi, vi aspetto in live!`],
         thumbnail:'/jpeg/squad/value.jpeg' 
     },
 ];
