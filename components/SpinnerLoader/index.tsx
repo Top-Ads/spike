1ffc5d8d7888f6a7051f4def71dafa9aa7aebebf
mchouaha <m.chouaha@gmail.com>
2021-06-11 13:16:27 +0200

    (feature) add spinner loader to slot and favorite cards

---
+++ components/SpinnerLoader/index.tsx
@@ -0,0 +1,33 @@
+import React, { FunctionComponent } from 'react'
+import { Fragment } from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { createStyles, withStyles } from '@material-ui/core';
+
+type PageProps = {
+    show: boolean
+};
+
+const CircularProgressStyled = withStyles(() =>
+  createStyles({
+    root: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        margin: 'auto',
+        color: '#e2b96d',
+        zIndex: 2
+    },
+  }),
+)(CircularProgress)
+
+const SpinnerLoader: FunctionComponent<PageProps> = ({show}) => {
+    return (
+        <Fragment>
+            {show && <CircularProgressStyled size={'30px'}/>}
+        </Fragment>
+    )
+} 
+
+export default SpinnerLoader
