cd825d040490876f242ac804ecf1ff343f14775f
mchouaha <m.chouaha@gmail.com>
2021-06-15 20:48:50 +0200

    integrate service-worker

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-module.exports = {
-    images: {
-      domains: [
-        'images.ctfassets.net',
-        'spike-images.s3.eu-central-1.amazonaws.com',
-        'casino-squad.b-cdn.net',
-        'casino-squad.com'
-      ],
-    },
-  };
+
+const withWorkbox = require("next-with-workbox")
+
+module.exports = withWorkbox({
+  workbox: {
+    swSrc:'/public/service-worker.js'
+  },
+  images: {
+    domains: [
+      'images.ctfassets.net',
+      'spike-images.s3.eu-central-1.amazonaws.com',
+      'casino-squad.b-cdn.net',
+      'casino-squad.com'
+    ],
+  },
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, FunctionComponent, useEffect, useState } from "react"
 import { ThemeProvider } from "styled-components"
+import { Workbox } from "workbox-window"
 import { DislikedSlotContext } from "../contexts"
 import { GlobalStyle, styledTheme } from "../theme"
 
@@ -11,8 +12,25 @@ interface IProps {
 export default function App( {Component, pageProps}: IProps) {
     
     useEffect(()=> {
+        
+        /* if ('serviceWorker' in navigator) {
+            // Use the window load event to keep the page load performant
+            window.addEventListener('load', () => {
+              navigator.serviceWorker.register('/service-worker.js');
+            });
+        } */
+
         if ( localStorage.getItem('favorites') === null ) 
-            localStorage.setItem('favorites', '[]') 
+            localStorage.setItem('favorites', '[]')
+
+        if (!("serviceWorker" in navigator) || process.env.NODE_ENV !== "production") {
+            console.warn("Pwa support is disabled")
+            return
+        }
+      
+        const wb = new Workbox("/public/service-worker.js", { scope: "/" })
+        wb.register()
+
     }, [])
 
     const ContextProvider: FunctionComponent = ({ children }) => {
