015cfb8ce8ac07f7100053d6fd1436151e1425a3
mchouaha <m.chouaha@gmail.com>
2021-06-16 13:55:41 +0200

    (feature) - add service worker for pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
+const withWorkbox = require("next-with-workbox");
+
 module.exports = {
-    images: {
-      domains: [
-        'images.ctfassets.net',
-        'spike-images.s3.eu-central-1.amazonaws.com',
-        'casino-squad.b-cdn.net',
-        'casino-squad.com'
-      ],
-    },
+    
   };
+
+module.exports = withWorkbox({
+  images: {
+    domains: [
+      'images.ctfassets.net',
+      'spike-images.s3.eu-central-1.amazonaws.com',
+      'casino-squad.b-cdn.net',
+      'casino-squad.com'
+    ],
+  },
+  workbox: {
+    dest: "public",
+    swDest: "sw.js",
+    swSrc: "public/worker.js",
+    force: true,
+    maximumFileSizeToCacheInBytes: 10000000
+  },
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, FunctionComponent, useEffect, useState } from "react"
 import { ThemeProvider } from "styled-components"
+import { Workbox } from "workbox-window"
 import { DislikedSlotContext } from "../contexts"
 import { GlobalStyle, styledTheme } from "../theme"
 
@@ -12,7 +13,23 @@ export default function App( {Component, pageProps}: IProps) {
     
     useEffect(()=> {
         if ( localStorage.getItem('favorites') === null ) 
-            localStorage.setItem('favorites', '[]') 
+            localStorage.setItem('favorites', '[]')
+        
+        if("serviceWorker" in navigator) {
+            window.addEventListener("load", function () {
+                /* navigator.serviceWorker.register("/sw.js", {scope: "/"}).then(
+                function (registration) {
+                    console.log("Service Worker registration successful with scope: ", registration.scope);
+                },
+                function (err) {
+                    console.log("Service Worker registration failed: ", err);
+                }
+                ); */
+
+                const wb = new Workbox("/sw.js", { scope: "/" })
+                wb.register()
+            });
+        }
     }, [])
 
     const ContextProvider: FunctionComponent = ({ children }) => {
