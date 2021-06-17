6067206f61915234e8923ef5c9c74da6dedd91bb
mchouaha <m.chouaha@gmail.com>
2021-06-17 13:54:14 +0200

    (fix) - edit manifest pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles'
+import { CDN } from '../public/environment'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -38,10 +39,14 @@ export default class MyDocument extends Document {
     return (
         <Html lang="it">
             <Head>
-              <link rel="preconnect" href="https://casino-squad.b-cdn.net/" />
-              <link rel="dns-prefetch" href="https://casino-squad.b-cdn.net/" />
-              <link rel="apple-touch-icon" href="/public/apple_icon.png"></link>
+              <link rel="preconnect" href={`${CDN}`}/>
+              <link rel="dns-prefetch" href={`${CDN}`}/>
+             {/*  <link rel="apple-touch-icon" sizes="192x192" href={`${CDN}/png/touch_icon_iphone`}/>
+              <link rel="apple-touch-icon" sizes="152x152" href={`${CDN}/png/touch_icon_ipad`}/>
+              <link rel="apple-touch-startup-image" sizes="640x1136" href="/apple-spash_640.png"/> 
+              <meta name="apple-mobile-web-app-status-bar-style" content="#e2b96d"/>*/}
             </Head>
+
             <body>
                 <Main />
                 <NextScript />
