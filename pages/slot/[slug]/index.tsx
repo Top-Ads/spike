d83988437869e0977b71b962bb2e497de353c390
mchouaha <m.chouaha@gmail.com>
2021-10-28 15:37:38 +0200

    remove provider name on similar slots [slot page]

--- pages/slot/[slug]/index.tsx
+++ pages/slot/[slug]/index.tsx
@@ -283,7 +283,7 @@ const SlotPage: FunctionComponent<PageProps> = ({data}) => {
                 </Main>
 
                 <div className="layout-container">
-                    <h2 style={{textTransform: 'uppercase'}}> GIOCHI SIMILI A QUESTO - {data.producer.name}</h2>
+                    <h2 style={{textTransform: 'uppercase'}}> GIOCHI SIMILI A QUESTO </h2>
                     <Divider/>
 
                     <GridContainer>
