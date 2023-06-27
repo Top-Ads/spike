b583b6e9d89257249951c6279c35c285d877974e
davidevaliante <dav.valiante@gmail.com>
2023-06-27 16:13:53 +0200

    changed 888 bonus link

--- lib/utils/constants.ts
+++ lib/utils/constants.ts
@@ -1,79 +1,79 @@
 export enum Category {
-	FAVORITES = 'preferiti',
+  FAVORITES = "preferiti",
 }
 
 export enum GridType {
-	SLOTS,
-	GIOCHISLOTS,
-	TOPBONUS,
-	BONUS,
-	QUESTIONS,
-	FREE,
-	STATS,
-	PRODUCER,
+  SLOTS,
+  GIOCHISLOTS,
+  TOPBONUS,
+  BONUS,
+  QUESTIONS,
+  FREE,
+  STATS,
+  PRODUCER,
 }
 
 export enum SlotType {
-	NEW,
-	ONLINE = 'online',
-	BAR = 'rtp',
-	VLT = 'vlt',
+  NEW,
+  ONLINE = "online",
+  BAR = "rtp",
+  VLT = "vlt",
 }
 
 export enum SlotFilterList {
-	RTP = 'RTP',
-	UPDATED_AT = 'Data Aggiornamento',
-	CREATED_AT = 'Data pubblicazione',
-	NAME = 'Nome',
-	SHUFFLE = 'Ordina',
+  RTP = "RTP",
+  UPDATED_AT = "Data Aggiornamento",
+  CREATED_AT = "Data pubblicazione",
+  NAME = "Nome",
+  SHUFFLE = "Ordina",
 }
 
 export enum LiveStats {
-	MONOPOLY,
-	CRAZYTIME,
-	DREAMCATCHER,
+  MONOPOLY,
+  CRAZYTIME,
+  DREAMCATCHER,
 }
 
 export enum SymbolLayout {
-	CARD,
-	TABLE,
+  CARD,
+  TABLE,
 }
 
 export const PAGE_BONUSES = [
-	'LeoVegas',
-	'StarCasinò',
-	'Starvegas',
-	'BetRoom',
-	'WinCasino',
-	'PokerStars Casino',
-	'NetBet',
-	'GoldBet',
-	'888 Casino',
-	'King Casino',
-	'Eurobet',
-	'Betway',
-	'Gioco Digitale',
-]
+  "LeoVegas",
+  "StarCasinò",
+  "Starvegas",
+  "BetRoom",
+  "WinCasino",
+  "PokerStars Casino",
+  "NetBet",
+  "GoldBet",
+  "888 Casino",
+  "King Casino",
+  "Eurobet",
+  "Betway",
+  "Gioco Digitale",
+];
 
 export const pageBonusesRemapping: any = {
-	LeoVegas:
-		'https://ntrfr.leovegas.com/redirect.aspx?pid=3708703&lpid=1757&bid=19140',
-	StarCasinò:
-		'http://record.affiliatelounge.com/_SEA3QA6bJTMP_fzV1idzxmNd7ZgqdRLk/135/',
-	BetRoom:
-		'https://www.promovt.info/betroomcasino/?page=user&p=registration&mp=b76750fa-ea90-424c-85d2-00e33416391e',
-	Starvegas: 'https://www.starvegas.it/gmg/refer/61782b177358340001a18ac7',
-	WinCasino:
-		'https://vincipromo.it/wincasino/?mp=42794b32-7604-49d2-92d0-8adf67a6b173',
-	GoldBet: 'https://media.goldbetpartners.it/redirect.aspx?pid=5116&bid=1495',
-	NetBet: 'https://banners.livepartners.com/view.php?z=151484',
-	'888 Casino': 'https://ic.aff-handler.com/c/43431?sr=1864253',
-	'King Casino': 'http://cs.kingcasino.it/',
-	Eurobet:
-		'https://record.betpartners.it/_E_C7XwxgprAZV93hC2dJ_GNd7ZgqdRLk/113/',
-	Betway: 'https://partners.betway.it/bwp/casino/?s=bpi29951&a=bpiadid167219',
-	'Gioco Digitale':
-		'https://mediaserver.entainpartners.com/renderBanner.do?zoneId=2031706',
-	'PokerStars Casino':
-		'https://secure.starsaffiliateclub.com/C.ashx?btag=a_186177b_6907c_&affid=100976968&siteid=186177&adid=6907&c=',
-}
+  LeoVegas:
+    "https://ntrfr.leovegas.com/redirect.aspx?pid=3708703&lpid=1757&bid=19140",
+  StarCasinò:
+    "http://record.affiliatelounge.com/_SEA3QA6bJTMP_fzV1idzxmNd7ZgqdRLk/135/",
+  BetRoom:
+    "https://www.promovt.info/betroomcasino/?page=user&p=registration&mp=b76750fa-ea90-424c-85d2-00e33416391e",
+  Starvegas: "https://www.starvegas.it/gmg/refer/61782b177358340001a18ac7",
+  WinCasino:
+    "https://vincipromo.it/wincasino/?mp=42794b32-7604-49d2-92d0-8adf67a6b173",
+  GoldBet: "https://media.goldbetpartners.it/redirect.aspx?pid=5116&bid=1495",
+  NetBet: "https://banners.livepartners.com/view.php?z=151484",
+  "888 Casino": "https://ic.aff-handler.com/c/48223?sr=1864253",
+  "King Casino": "http://cs.kingcasino.it/",
+  Eurobet:
+    "https://record.betpartners.it/_E_C7XwxgprAZV93hC2dJ_GNd7ZgqdRLk/113/",
+  Betway: "https://partners.betway.it/bwp/casino/?s=bpi29951&a=bpiadid167219",
+  "Gioco Digitale":
+    "https://mediaserver.entainpartners.com/renderBanner.do?zoneId=2031706",
+  "PokerStars Casino":
+    "https://secure.starsaffiliateclub.com/C.ashx?btag=a_186177b_6907c_&affid=100976968&siteid=186177&adid=6907&c=",
+};
